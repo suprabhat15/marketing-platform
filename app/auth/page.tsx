3057fce050af4f360508dd8ac1c6b828fdd95b7f
suprabhatkumar <suprabhat2018@gmail.com>
2026-01-30 10:30:16 +0530

    chore: uncomment signup, signin via google

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -135,7 +135,7 @@ export default function AuthPage() {
             <Tabs defaultValue="login" className="w-full">
               <TabsList className="grid w-full grid-cols-2">
                 <TabsTrigger value="login">Sign In</TabsTrigger>
-                {/* <TabsTrigger value="signup">Sign Up</TabsTrigger> */}
+                <TabsTrigger value="signup">Sign Up</TabsTrigger>
               </TabsList>
 
               <TabsContent value="login" className="space-y-4">
@@ -263,7 +263,7 @@ export default function AuthPage() {
                 </div>
               </div>
 
-              {/* <Button
+              <Button
                 type="button"
                 variant="outline"
                 className="mt-4 w-full"
@@ -272,7 +272,7 @@ export default function AuthPage() {
               >
                 <Mail className="mr-2 h-4 w-4" />
                 Google
-              </Button> */}
+              </Button>
             </div>
 
             {error && (
